04880b1335cf3d0f91578d19e61bb792200f9be5
Victor <victorlp03@gmail.com>
2024-07-10 14:22:30 -0400

    updated main.py

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///finance_tracker.db'
 db.init_app(app)
 bcrypt = Bcrypt(app)
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'login'      #specifies the route to redirect users if they need to login
 
+# This callback is used to reload the user object from the user ID stored in the session.
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -29,23 +30,25 @@ def register():
         # flashes a success message after account is created
         flash('Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('login'))       # if entry is valid then redirect to login page
-    return render_template('register.html', form=form)      #this prints HTML to the webpage
+    return render_template('register.html', form=form)      
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # if user is authenticated the this prevents logged-in users from accessing the login page again and redirects them to home 
     if current_user.is_authenticated:
         return redirect(url_for('home'))
     form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=True)
-            return redirect(url_for('home'))
+    if form.validate_on_submit():       #checks if entries in login form are valid
+        user = User.query.filter_by(email=form.email.data).first()      #queries the User table to find the first user with the email address entered in the form. If not then user is None
+        if user and bcrypt.check_password_hash(user.password, form.password.data):      #entered password is compared to the hashed password stored in the database
+            login_user(user, remember=True)         #login_user logs in a user and the remember=True saves a cookie will on the user’s computer to remember the user
+            return redirect(url_for('home'))        #redirects to homepage once logged in
         else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
+            flash('Login Unsuccessful. Please check email and password', 'danger')      #if invalid login then flash message
     return render_template('login.html', form=form)
 
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('home'))
@@ -53,8 +56,8 @@ def logout():
 @app.route('/', methods=['GET'])
 @login_required
 def home():
-    transactions = Transaction.query.filter_by(user_id=current_user.id).all()
-    total_balance = sum(t.amount for t in transactions)
+    transactions = Transaction.query.filter_by(user_id=current_user.id).all()       #queries all the transactions for the current user
+    total_balance = sum(t.amount for t in transactions)         #adds them up to display total amount in the home page
     return render_template('home.html', transactions=transactions, total_balance=total_balance)
 
 @app.route('/add', methods=['GET', 'POST'])
@@ -62,11 +65,12 @@ def home():
 def add_transaction():
     form = TransactionForm()
     if form.validate_on_submit():
+        # sends form data to database
         new_transaction = Transaction(description=form.description.data, amount=form.amount.data, currency=form.currency.data, user_id=current_user.id)
         db.session.add(new_transaction)
         db.session.commit()
-        flash('Transaction added successfully!', 'success')
-        return redirect(url_for('home'))
+        flash('Transaction added successfully!', 'success')     #if valid then flash method
+        return redirect(url_for('add_transaction'))         #redirect to same page to reset form entries
     return render_template('add_transaction.html', form=form)
 
 if __name__ == '__main__':
@@ -74,3 +78,5 @@ if __name__ == '__main__':
         db.create_all()
     app.run(debug=True)
 
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
